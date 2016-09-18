96ac412c431aa5eb3d104fa06364b5a8bc9a0b3f
Eric Paniagua <eric.paniagua@gmail.com>
2016-09-18 01:42:24 -0700

    Allow open terms so we can drop Maybe from return types.

--- utlc_alg.hs
+++ utlc_alg.hs
@@ -14,58 +14,59 @@ data UTLCTerm = Var DbIdx               -- a DeBruijn index representation of
               | Lam UTLCTerm            -- a lambda abstracted term; or
               | App UTLCTerm UTLCTerm   -- an application of one term to another.
   deriving (Eq,Show)
--- Note: all terms defined this way type check trivially, as there is only
---  one type, left implicit.
--- Note: this encoding admits open terms, but we will treat these an invalid
---  in what follows.
+-- Note: all terms defined this way type check trivially, as there is implicitly
+--  only one type.
+-- Note: this encoding admits open terms, which may or may not be a good idea,
+--  but allows slight simplification of the code below.
 
 -- Ctx represents the terms bound by any lambda abstractions wrapping the
 --  current term.  As we perform a beta reduction, the argument (Just arg) is
 --  prepended to the Ctx for consistency with the use of DeBruijn indices.
 -- Nothing is used as a placeholder to keep indices lined up when performing
---  recursive substitution.
+--  recursive substitution in lambda abstractions.
 type Ctx = [Maybe UTLCTerm]
 
 -- lkup obtains from the Ctx the UTLCTerm by the DeBruijn index.  The
 --  possibility that the DbIdx is free in the term is handled by wrapping the
 --  result in the Maybe monad.
 lkup :: Ctx -> DbIdx -> Maybe (Maybe UTLCTerm)
-lkup []     _       = Nothing       -- The DbIdx is free.
+lkup []     _       = Nothing       -- The DbIdx is free. This information is
+                                    --  is not used below, but there's nothing
+                                    --  else sensible to return here.
 lkup (a:as) O       = Just a        -- Found the term bound to DbIdx.
 lkup (a:as) (S idx) = lkup as idx   -- Keep looking.
 
 -- Perform substitutions from a fixed context.
--- In particular, do not perofrm beta reduction.
-subst :: Ctx -> UTLCTerm -> Maybe UTLCTerm
-subst ctx (Var idx)      = do val <- lkup ctx idx
-                              Just $ case val of
-                                Nothing -> Var idx
-                                Just term -> term
-subst ctx (Lam body)     = do body' <- subst (Nothing:ctx) body
-                              return $ Lam body'
-subst ctx (App term arg) = do term' <- subst ctx term
-                              arg' <- subst ctx arg
-                              return $ App term' arg'
+-- In particular, do not perform beta reduction.
+subst :: Ctx -> UTLCTerm -> UTLCTerm
+-- The Var case became more complicated in exchange for dropping the Maybe
+-- monad wrapping the return value of these evaluation functions.
+subst ctx (Var idx)      = let val = do val <- lkup ctx idx
+                                        return $ case val of
+                                          Nothing -> Var idx
+                                          Just term -> term
+                           in case val of
+                            Nothing -> Var idx
+                            Just val' -> val'
+subst ctx (Lam body)     = Lam $ subst (Nothing:ctx) body
+subst ctx (App term arg) = App (subst ctx term) (subst ctx arg)
 
--- Reduces a closed term to WHNF and an open term to Nothing.
-whnfc :: Ctx -> UTLCTerm -> Maybe UTLCTerm
+-- Reduces a term to WHNF.
+whnfc :: Ctx -> UTLCTerm -> UTLCTerm
 -- Finds the outermost redex and performs a single beta reduction.
 whnfc ctx (App (Lam body) arg) = subst (Just arg:ctx) body
-whnfc ctx (App term       arg) = do left <- whnfc ctx term
-                                    subst ctx $ App left arg
+whnfc ctx (App term       arg) = subst ctx $ App (whnfc ctx term) arg
 -- Var and Lam are already in WHNF, so we just apply any substitutions.
 whnfc ctx term                 = subst ctx term
 
 -- WHNF wrapper that starts with an empty context.
-whnf :: UTLCTerm -> Maybe UTLCTerm
+whnf :: UTLCTerm -> UTLCTerm
 whnf = whnfc []
 
 -- Beta reduce as many times as possible.
-eval :: UTLCTerm -> Maybe UTLCTerm
-eval term = do term' <- whnf term
-               if term == term'
-               then return term
-               else eval term'
+eval :: UTLCTerm -> UTLCTerm
+eval term = if term == term' then term else eval term'
+  where term' = whnf term
 
 -- SKI combinator calculus
 i :: UTLCTerm
@@ -125,87 +126,87 @@ impl = Lam $ Lam $ App (App or (App neg (Var (S O)))) (Var O)
 -- Tests
 whnf_tests = "whnf" ~: [
   "refl" ~: [
-    "i" ~: (whnf i) @?= Just i,
-    "k" ~: (whnf k) @?= Just k,
-    "s" ~: (whnf s) @?= Just s,
-    "y" ~: (whnf y) @?= Just y],
+    "i" ~: (whnf i) @?= i,
+    "k" ~: (whnf k) @?= k,
+    "s" ~: (whnf s) @?= s,
+    "y" ~: (whnf y) @?= y],
   "i" ~: [
-    "i i" ~: (whnf $ App i i) @?= Just i,
-    "i k" ~: (whnf $ App i k) @?= Just k,
-    "i s" ~: (whnf $ App i s) @?= Just s,
-    "i y" ~: (whnf $ App i y) @?= Just y],
+    "i i" ~: (whnf $ App i i) @?= i,
+    "i k" ~: (whnf $ App i k) @?= k,
+    "i s" ~: (whnf $ App i s) @?= s,
+    "i y" ~: (whnf $ App i y) @?= y],
   "k" ~: [
-    "k i" ~: (whnf $ App k i) @?= Just (Lam i),
-    "k k" ~: (whnf $ App k k) @?= Just (Lam k),
-    "k s" ~: (whnf $ App k s) @?= Just (Lam s),
-    "k y" ~: (whnf $ App k y) @?= Just (Lam y),
-    "k i i" ~: (whnf $ App (App k i) i) @?= Just (App (Lam i) i),
-    "k i k" ~: (whnf $ App (App k i) k) @?= Just (App (Lam i) k),
-    "k i s" ~: (whnf $ App (App k i) s) @?= Just (App (Lam i) s),
-    "k i y" ~: (whnf $ App (App k i) y) @?= Just (App (Lam i) y)]]
+    "k i" ~: (whnf $ App k i) @?= Lam i,
+    "k k" ~: (whnf $ App k k) @?= Lam k,
+    "k s" ~: (whnf $ App k s) @?= Lam s,
+    "k y" ~: (whnf $ App k y) @?= Lam y,
+    "k i i" ~: (whnf $ App (App k i) i) @?= App (Lam i) i,
+    "k i k" ~: (whnf $ App (App k i) k) @?= App (Lam i) k,
+    "k i s" ~: (whnf $ App (App k i) s) @?= App (Lam i) s,
+    "k i y" ~: (whnf $ App (App k i) y) @?= App (Lam i) y]]
 eval_tests = "eval" ~: [
   "refl" ~: [
-    "i" ~: (eval i) @?= Just i,
-    "k" ~: (eval k) @?= Just k,
-    "s" ~: (eval s) @?= Just s,
-    "y" ~: (eval y) @?= Just y],
+    "i" ~: (eval i) @?= i,
+    "k" ~: (eval k) @?= k,
+    "s" ~: (eval s) @?= s,
+    "y" ~: (eval y) @?= y],
   "i" ~: [
-    "i i" ~: (eval $ App i i) @?= Just i,
-    "i k" ~: (eval $ App i k) @?= Just k,
-    "i s" ~: (eval $ App i s) @?= Just s,
-    "i y" ~: (eval $ App i y) @?= Just y],
+    "i i" ~: (eval $ App i i) @?= i,
+    "i k" ~: (eval $ App i k) @?= k,
+    "i s" ~: (eval $ App i s) @?= s,
+    "i y" ~: (eval $ App i y) @?= y],
   "k" ~: [
-    "k i" ~: (eval $ App k i) @?= Just (Lam i),
-    "k k" ~: (eval $ App k k) @?= Just (Lam k),
-    "k s" ~: (eval $ App k s) @?= Just (Lam s),
-    "k y" ~: (eval $ App k y) @?= Just (Lam y),
-    "k i i" ~: (eval $ App (App k i) i) @?= Just i,
-    "k i k" ~: (eval $ App (App k i) k) @?= Just i,
-    "k i s" ~: (eval $ App (App k i) s) @?= Just i,
-    "k i y" ~: (eval $ App (App k i) y) @?= Just i],
+    "k i" ~: (eval $ App k i) @?= (Lam i),
+    "k k" ~: (eval $ App k k) @?= (Lam k),
+    "k s" ~: (eval $ App k s) @?= (Lam s),
+    "k y" ~: (eval $ App k y) @?= (Lam y),
+    "k i i" ~: (eval $ App (App k i) i) @?= i,
+    "k i k" ~: (eval $ App (App k i) k) @?= i,
+    "k i s" ~: (eval $ App (App k i) s) @?= i,
+    "k i y" ~: (eval $ App (App k i) y) @?= i],
   "multi" ~: [
-    "s k s k" ~: (eval $ App (App (App s k) s) k) @?= Just k]]
+    "s k s k" ~: (eval $ App (App (App s k) s) k) @?= k]]
 iota_tests = "iota" ~: [
   "i" ~: [
-    "iota_i i" ~: (eval $ App iota_i i) @?= Just i,
-    "iota_i k" ~: (eval $ App iota_i k) @?= Just k,
-    "iota_i s" ~: (eval $ App iota_i s) @?= Just s,
-    "iota_i y" ~: (eval $ App iota_i y) @?= Just y,
+    "iota_i i" ~: (eval $ App iota_i i) @?= i,
+    "iota_i k" ~: (eval $ App iota_i k) @?= k,
+    "iota_i s" ~: (eval $ App iota_i s) @?= s,
+    "iota_i y" ~: (eval $ App iota_i y) @?= y,
     "iota_i iota_i" ~: (eval $ App iota_i iota_i) @?= eval iota_i],
   "k" ~: [
-    "iota_k i" ~: (eval $ App iota_k i) @?= Just (Lam i),
-    "iota_k k" ~: (eval $ App iota_k k) @?= Just (Lam k),
-    "iota_k s" ~: (eval $ App iota_k s) @?= Just (Lam s),
-    "iota_k y" ~: (eval $ App iota_k y) @?= Just (Lam y),
-    "iota_k i i" ~: (eval $ App (App iota_k i) i) @?= Just i,
-    "iota_k i k" ~: (eval $ App (App iota_k i) k) @?= Just i,
-    "iota_k i s" ~: (eval $ App (App iota_k i) s) @?= Just i,
-    "iota_k i y" ~: (eval $ App (App iota_k i) y) @?= Just i]]
+    "iota_k i" ~: (eval $ App iota_k i) @?= Lam i,
+    "iota_k k" ~: (eval $ App iota_k k) @?= Lam k,
+    "iota_k s" ~: (eval $ App iota_k s) @?= Lam s,
+    "iota_k y" ~: (eval $ App iota_k y) @?= Lam y,
+    "iota_k i i" ~: (eval $ App (App iota_k i) i) @?= i,
+    "iota_k i k" ~: (eval $ App (App iota_k i) k) @?= i,
+    "iota_k i s" ~: (eval $ App (App iota_k i) s) @?= i,
+    "iota_k i y" ~: (eval $ App (App iota_k i) y) @?= i]]
 logic_tests = "logic" ~: [
   "neg" ~: [
-    "tt" ~: (eval $ App neg tt) @?= Just ff,
-    "ff" ~: (eval $ App neg ff) @?= Just tt],
+    "tt" ~: (eval $ App neg tt) @?= ff,
+    "ff" ~: (eval $ App neg ff) @?= tt],
   "doubleneg" ~: [
-    "tt" ~: (eval $ App neg (App neg tt)) @?= Just tt,
-    "ff" ~: (eval $ App neg (App neg ff)) @?= Just ff],
+    "tt" ~: (eval $ App neg (App neg tt)) @?= tt,
+    "ff" ~: (eval $ App neg (App neg ff)) @?= ff],
   "cond" ~: [
-    "tt" ~: (eval $ App (App (App cond tt) tt) ff) @?= Just tt,
-    "ff" ~: (eval $ App (App (App cond ff) tt) ff) @?= Just ff],
+    "tt" ~: (eval $ App (App (App cond tt) tt) ff) @?= tt,
+    "ff" ~: (eval $ App (App (App cond ff) tt) ff) @?= ff],
   "and" ~: [
-    "tt tt" ~: (eval $ App (App and tt) tt) @?= Just tt,
-    "tt ff" ~: (eval $ App (App and tt) ff) @?= Just ff,
-    "ff tt" ~: (eval $ App (App and ff) tt) @?= Just ff,
-    "ff ff" ~: (eval $ App (App and ff) ff) @?= Just ff],
+    "tt tt" ~: (eval $ App (App and tt) tt) @?= tt,
+    "tt ff" ~: (eval $ App (App and tt) ff) @?= ff,
+    "ff tt" ~: (eval $ App (App and ff) tt) @?= ff,
+    "ff ff" ~: (eval $ App (App and ff) ff) @?= ff],
   "or" ~: [
-    "tt tt" ~: (eval $ App (App or tt) tt) @?= Just tt,
-    "tt ff" ~: (eval $ App (App or tt) ff) @?= Just tt,
-    "ff tt" ~: (eval $ App (App or ff) tt) @?= Just tt,
-    "ff ff" ~: (eval $ App (App or ff) ff) @?= Just ff],
+    "tt tt" ~: (eval $ App (App or tt) tt) @?= tt,
+    "tt ff" ~: (eval $ App (App or tt) ff) @?= tt,
+    "ff tt" ~: (eval $ App (App or ff) tt) @?= tt,
+    "ff ff" ~: (eval $ App (App or ff) ff) @?= ff],
   "impl" ~: [
-    "tt tt" ~: (eval $ App (App impl tt) tt) @?= Just tt,
-    "tt ff" ~: (eval $ App (App impl tt) ff) @?= Just ff,
-    "ff tt" ~: (eval $ App (App impl ff) tt) @?= Just tt,
-    "ff ff" ~: (eval $ App (App impl ff) ff) @?= Just tt]]
+    "tt tt" ~: (eval $ App (App impl tt) tt) @?= tt,
+    "tt ff" ~: (eval $ App (App impl tt) ff) @?= ff,
+    "ff tt" ~: (eval $ App (App impl ff) tt) @?= tt,
+    "ff ff" ~: (eval $ App (App impl ff) ff) @?= tt]]
 tests = "algebraic untyped lambda calculus" ~: [whnf_tests, eval_tests, iota_tests, logic_tests]
 
 main :: IO ()
